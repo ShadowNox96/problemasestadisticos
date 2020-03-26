e01c173940c3f0dfcb3205571532eba58381ef31
shadownox <shadownox@pop-os.localdomain>
2020-03-26 16:53:05 -0600

    Prueba linux

--- app.py
+++ app.py
@@ -1,96 +1,103 @@
-#Importamos la libreria 
+# Importamos la libreria
+# Para el proyecto
 
 from flask import Flask, render_template, redirect, url_for, request, flash, make_response
-from math import factorial,sqrt 
+from math import factorial, sqrt
 import numpy as np
 import funciones as fn
-#Se crea un objeto app de flask, es el objeto inicial con el cual se va a lanzar el framework 
+# Se crea un objeto app de flask, es el objeto inicial con el cual se va a lanzar el framework
 app = Flask(__name__)
-#Siempre es necesario inicializar la sesion para que no existan erroes 
-app.secret_key= 'mysecretkey'
-#Decorador para definir la ruta para la pagina principal 
+# Siempre es necesario inicializar la sesion para que no existan erroes
+app.secret_key = 'mysecretkey'
+# Decorador para definir la ruta para la pagina principal
 @app.route("/")
-#Se define una funcion para la ruta de la pagina principal 
+# Se define una funcion para la ruta de la pagina principal
 def indexPage():
-    #Lo necesario para enlazar las templates es utilizar el recurso render template 
+    # Lo necesario para enlazar las templates es utilizar el recurso render template
     return render_template("index.html")
+
 
 @app.route("/binomial")
 def fnBinomial():
     return render_template("binomial.html")
 
+
 @app.route("/hiper")
 def fnHiper():
     return render_template("hipergeometrica.html")
 
-@app.route("/calcularBinomial", methods = ['POST'])
+
+@app.route("/calcularBinomial", methods=['POST'])
 def calcularBinomial():
     media = 0
     desv = 0
     fc = 0
     sesgo = 0
     curtosis = 0
-    mediana =0
+    mediana = 0
     if request.method == 'POST':
-        #Extraigo los parametros del formulario
+        # Extraigo los parametros del formulario
         n = int(request.form['muestra'])
         x1 = int(request.form['x1'])
         x2 = int(request.form['x2'])
         p = float(request.form['p'])
         q = 100-p
         try:
-            #trata de extraer el valor de N
+            # trata de extraer el valor de N
             N = 0
-            N= int(request.form['N'])
+            N = int(request.form['N'])
         except:
             # No hay valor de N hacer con poblacion infinita
-            media = n*p/100
-            desv = sqrt(n*p*q)/100
+        media = n*p/100
+        desv = sqrt(n*p*q)/100
 
         if N != 0 and (n/N)*100 > 5:
-            #EsPoblacion Infinita
+            # EsPoblacion Infinita
             media = n*p/100
             desv = sqrt(n*p*q)/100
 
         elif N != 0 and (n/N)*100 <= 5:
-            #Es poblacion Finita
+            # Es poblacion Finita
             media = n*p/100
             fc = sqrt((N-n)/(N-1))
             desv = fc * sqrt(n*p*q)/100
 
-        #Empiezo la grafica
+        # Empiezo la grafica
         grafica = '{"cols": [{"id":"","label":"X","pattern":"","type":"number"},{"id":"","label":"P","pattern":"","type":"number"}],"rows": ['
 
-        #Verifico que la muestra sea mayor que los numeros de exitos deseados 
+        # Verifico que la muestra sea mayor que los numeros de exitos deseados
         if n >= x1 and x2:
-            #Verifico que la suma de las probabilidades de exito y fracaso sean iguales a 100
-           
+            # Verifico que la suma de las probabilidades de exito y fracaso sean iguales a 100
+
             rango = []
-            resultados =[] 
+            resultados = []
             suma = 0
             p = p/100
             q = q/100
-            rango = range(x1,x2+1,1)
-            
-            #Calculo de la mediana
+            rango = range(x1, x2+1, 1)
+
+            # Calculo de la mediana
             mediana = np.median(rango)
-                
-            #Recorro el rango y calculo la probabilidad
+
+            # Recorro el rango y calculo la probabilidad
             for x in rango:
-                n_x = n-x  
-                resultado = (factorial(n) / (factorial(x) * factorial(n-x))) * (p**x) * (q**n_x)
-                #Sumo cada uno de los resultados
+                n_x = n-x
+                resultado = (factorial(n) / (factorial(x) *
+                                             factorial(n-x))) * (p**x) * (q**n_x)
+                # Sumo cada uno de los resultados
                 suma = suma + resultado
-                # agrego cada resultado al array 
+                # agrego cada resultado al array
                 resultados.append([x, resultado])
-                #le agrego la parte de los datos al array de la grafica
-                grafica = grafica + '{"c":[{"v":'+str(x)+',"f":null},{"v":'+str(resultado)+',"f":null}]},'
+                # le agrego la parte de los datos al array de la grafica
+                grafica = grafica + \
+                    '{"c":[{"v":'+str(x)+',"f":null},{"v":' + \
+                    str(resultado)+',"f":null}]},'
             grafica = grafica + ']}'
 
-            #Calculo el sesgo y curtosis
-            sesgo,curtosis = fn.sesgoCurtosis(p,n)
-                
-            return render_template('resultadoBinomial.html', data = resultados, g = grafica, suma= suma, media= media, desv= desv, fc= fc, sesgo = sesgo, curtosis=curtosis, mediana= mediana)
+            # Calculo el sesgo y curtosis
+            sesgo, curtosis = fn.sesgoCurtosis(p, n)
+
+            return render_template('resultadoBinomial.html', data=resultados, g=grafica, suma=suma, media=media, desv=desv, fc=fc, sesgo=sesgo, curtosis=curtosis, mediana=mediana)
 
         else:
             flash('La suma del porcentaje de probabilidades no es igual a 100')
@@ -99,7 +106,8 @@ def calcularBinomial():
         flash('El valor menor o mayor de exitos deseados debe de ser menor al tamaño de la muestra ')
         return redirect(url_for('fnBinomial'))
 
-@app.route('/calculoHiper', methods = ['POST'])
+
+@app.route('/calculoHiper', methods=['POST'])
 def calcularHiper():
     if request.method == 'POST':
         n = int(request.form['muestra'])
@@ -108,45 +116,39 @@ def calcularHiper():
         x1 = int(request.form['x1'])
         x2 = int(request.form['x2'])
 
-        resultados,grafica,total= fn.probabilidadHiper(N,T,n,x1,x2)
-        media = fn.mediaHiper(n,N,T)
+        resultados, grafica, total = fn.probabilidadHiper(N, T, n, x1, x2)
+        media = fn.mediaHiper(n, N, T)
         desv = fn.desvHiper(n, T, N)
         p = media/n
-        sesgo, curtosis = fn.sesgoCurtosis(p,n)
-        return render_template('resultadoHiper.html', data = resultados, grafica=grafica, media=media, desv=desv,total=total, sesgo=sesgo, curtosis=curtosis)
+        sesgo, curtosis = fn.sesgoCurtosis(p, n)
+        return render_template('resultadoHiper.html', data=resultados, grafica=grafica, media=media, desv=desv, total=total, sesgo=sesgo, curtosis=curtosis)
+
 
 @app.route('/poisson')
 def poissonPage():
     return render_template('poisson.html')
 
-@app.route('/calcularPoisson', methods = ['POST'])
+
+@app.route('/calcularPoisson', methods=['POST'])
 def calcularPoisson():
     if request.method == 'POST':
         media = float(request.form['media'])
         x1 = int(request.form['x1'])
         x2 = int(request.form['x2'])
-        data,grafica,tProb = fn.probPoisson(x1,x2,media)
+        data, grafica, tProb = fn.probPoisson(x1, x2, media)
 
-        return render_template('resultPoisson.html', data= data, grafica = grafica, total = tProb )
+        return render_template('resultPoisson.html', data=data, grafica=grafica, total=tProb)
 
-@app.route('/cookie')            
+
+@app.route('/cookie')
 def cookie(grafica):
     res = make_response('Cookie Establecida')
     print(grafica)
-    res.set_cookie('nombre','Línea de Código')
+    res.set_cookie('nombre', 'Línea de Código')
     return res
-        
-        
 
 
-    
-
-
-
-
-
-
-#Ejemplos de lo antes trabajado 
+# Ejemplos de lo antes trabajado
 """
 @app.route('/otherPage')
 def otherPage():
@@ -170,14 +172,12 @@ def user(nombreUser):
 @app.route('/suma')
 def suma():
     return 'La suma es> 44' 
-"""    
+"""
 
 
-#Funcion principal 
+# Funcion principal
 if __name__ == "__main__":
-    #Ejecuta el objeto app 
-    #app.run()
-    #Para evitar recargar el levantado del servidor se usa
+    # Ejecuta el objeto app
+    # app.run()
+    # Para evitar recargar el levantado del servidor se usa
     app.run(debug=True)
-
-
